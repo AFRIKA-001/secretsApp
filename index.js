f7f6abc5f6e7c76cb81075d9b891ffd5cfbda52e
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-06-10 16:16:34 +0300

    practiced REST api using the AXIOS client Library

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import axios from "axios";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get('/', async (req,res) => {
+   
+    try {
+        const response = await axios.get( "https://secrets-api.appbrewery.com/random")
+        const data = await response.data;
+        res.render("index.ejs" , {secret: data.secret , user:data.username})
+    } catch (error) {
+        res.status(500).send("internal server error")
+    }
+  
+})
+
+
+app.listen(port ,()=>{
+    console.log(`the app is up and running in port ${port}`)
+});
+
+
+
+
+// HINTS:
+// 1. Import express and axios
+
+// 2. Create an express app and set the port number.
+
+// 3. Use the public folder for static files.
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
